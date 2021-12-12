25d52bc8557fbff869a76637004cdfc22727b9e1
Vika <vikaberezuk12345@gmail.com>
2021-12-12 22:02:04 +0200

    03_GuessNumber

---
+++ 03_GuessNumber/index.js
@@ -0,0 +1,101 @@
+document.body.style.background = 'green';
+document.body.style.color = 'white';
+const lastResult = document.querySelector('.lastResult');
+const guessErrorResult = document.querySelector('.guessErrorResult');
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+const guessMaxCount = document.getElementById('guessMaxCount');
+const guessMinNumber = document.getElementById('guessMinNumber');
+const guessMaxNumber = document.getElementById('guessMaxNumber');
+const guessSubmitExit = document.querySelector('.guessSubmitExit');
+let minNumber = 1;
+let maxNumber = 200;
+let counterNumber = 15;
+let guessCount = 1;
+let resetButton;
+let randomNumber;
+
+function checkRangeNumber() {
+    randomNumber = Math.floor(Math.random() * (guessMaxNumber.value - guessMinNumber.value)) + Number(guessMinNumber.value);
+    if (guessMinNumber.value >= 1 && guessMinNumber.value <= 200 &&
+        guessMaxNumber.value >= 1 && guessMaxNumber.value <= 200 &&
+        guessMaxCount.value >= 1 && guessMaxCount.value <= 15 ) {
+        minNumber = document.getElementById('minNumber').innerText = String(guessMinNumber.value);
+        maxNumber = document.getElementById('maxNumber').innerText = String(guessMaxNumber.value);
+        counterNumber = document.getElementById('counterNumber').innerText = String(guessMaxCount.value);
+        console.log(randomNumber);
+        guessCount = 1;
+        guessSubmit.addEventListener('click', checkGuess);
+    } else {
+        guessMinNumber.value = 1;
+        guessMaxNumber.value = 200;
+        guessMaxCount.value = 15;
+        lastResult.textContent ='Введите числа в диапазоне от 1 до 200 и количество попыток от 1 до  15';
+    }
+    console.log(guessMaxNumber.value, guessMinNumber.value, guessMaxCount.value, minNumber);
+}
+checkRangeNumber();
+guessSubmitExit.addEventListener('click', setGameOver);
+
+function checkGuess() {
+    const userGuess = Number(guessField.value);
+    //guesses.textContent += userGuess + ' ';
+    if(isNaN(userGuess)) {
+        lastResult.textContent = `Вы указали букву, укажите число в диапазоне ${minNumber} до ${maxNumber}`;
+        return userGuess;
+    }
+    if( userGuess < +minNumber ||  userGuess > +maxNumber) {
+        lastResult.textContent = `Вы не верно указали число, укажите число в диапазоне ${minNumber} до ${maxNumber}`;
+        console.log(userGuess, +minNumber);
+        return userGuess;
+    }
+    if (userGuess === randomNumber) {
+            lastResult.textContent = `Поздравляю! Вы угадали! За ${guessCount} попыток`;
+            lastResult.style.fontWeight = '900';
+            guessErrorResult.textContent = '';
+            setGameOver();
+    } else if (guessCount === guessMaxCount) {
+            lastResult.textContent = `!!!К сожалению, Вы не угадали. Игра завершина за ${guessCount} попыток!!!`;
+            guessErrorResult.textContent = '';
+            setGameOver();
+    } else {
+            lastResult.textContent = 'Вы Не угадали!';
+            if(userGuess < randomNumber) {
+                guessErrorResult.textContent = `Предыдущее число было больше! Попытка ${guessCount}` ;
+            } else if(userGuess > randomNumber) {
+                guessErrorResult.textContent = `Предыдущее число было меньше! Попытка ${guessCount}`;
+            }
+    }
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
+}
+
+//guessSubmit.addEventListener('click', checkGuess);
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    guessSubmitExit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.style.position = 'relative';
+    resetButton.style.left = '45%';
+    resetButton.textContent = 'Начать новую игру';
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+    guessCount = 1;
+    const result = document.querySelectorAll('.result p');
+    for (const resetPara of result) {
+        resetPara.textContent = '';
+    }
+
+    resetButton.parentNode.removeChild(resetButton);
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessSubmitExit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+    randomNumber = Math.floor(Math.random() * 200) + 1;
+}
